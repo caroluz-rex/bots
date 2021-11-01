035ac8e4a023b53a0aff102dbce40c1fe34faad9
zakharchenkosv@student.bmstu.ru <felix.wojchik@gmail.com>
2021-11-01 21:11:45 +0300

    da

---
+++ 9/config.py
@@ -0,0 +1,6 @@
+daChannelList = ["903844131510554664","903338786816143400","902798623941726235"]
+#You don't need to change anything under this
+daChannelDictionary = dict.fromkeys(daChannelList, "")
+
+def getScope():
+    return(daChannelDictionary)
